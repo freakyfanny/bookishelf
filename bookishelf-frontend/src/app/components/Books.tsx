341e7591a081ba854365425a6a3397257e5839db
Fanny Petersson Sällberg <fannypeterssons@icloud.com>
2025-03-06 18:53:10 +0100

    gives result now

--- bookishelf-frontend/src/app/components/Books.tsx
+++ bookishelf-frontend/src/app/components/Books.tsx
@@ -2,17 +2,25 @@
 
 import { useQuery } from '@tanstack/react-query';
 import BookCard from './BookCard';
+import AuthorCard from './AuthorCard';
 
 interface Book {
+    type: string;
+    key: string;
     title: string;
+    name: string;
     description: string;
     imageUrl: string;
     category: string;
+    topSubjects: string;
+    topWork: string;
+    birthDate: string;
+    deathDate: string;
     link: string;
 }
 
 const fetchBooks = async (searchQuery: string) => {
-    console.log("searchQuery ",searchQuery);
+    console.log("searchQuery ", searchQuery);
     const res = await fetch(`/api/search?searchParam=${searchQuery}`);
     if (!res.ok) {
         throw new Error('Error fetching books');
@@ -34,27 +42,31 @@ const Books: React.FC<BooksProps> = ({ searchQuery }) => {
     if (isLoading) return <p>Loading...</p>;
 
     if (isError) {
-        console.error(error); 
+        console.error(error);
         return <p>Error: {error instanceof Error ? error.message : 'Unknown error'}</p>;
     }
 
-    console.log(data);
 
-    const fetchedBooks: Book[] = data?.docs || []; // ändra om det behövs här
-
+    const fetchedBooks: Book[] = data; // ändra om det behövs här
+    console.log('data', data); 
     return (
-        <div className="container mt-5">
+        <div className="container flex mt-5">
             <div className="md:flex">
-                {fetchedBooks.map((book, index) => (
-                    <BookCard
+                {fetchedBooks.map((book, index) =>
+                    (book.type = "book") ? <BookCard
                         key={index}
                         title={book.title}
                         description={book.description}
                         imageUrl={book.imageUrl}
                         category={book.category}
                         link={book.link}
-                    />
-                ))}
+                    /> : <AuthorCard
+                        key={index}
+                        name={book.name}
+                        topSubjects={book.topSubjects}
+                        topWork={book.topWork}
+                        imageUrl={book.imageUrl} birthDate={book.birthDate} deathDate={book.deathDate} />
+                )}
             </div>
         </div>
     );
