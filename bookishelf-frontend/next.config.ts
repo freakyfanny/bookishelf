b83b1b6d02cd8281b9651732750221e8a82a04fd
Fanny Petersson Sällberg <fannypeterssons@icloud.com>
2025-03-17 21:42:34 +0100

    changing to use Next IMAGE instead, and adding next config

--- bookishelf-frontend/next.config.ts
+++ bookishelf-frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
+  images: {
+    domains: ['covers.openlibrary.org'],
+  },
 };
 
 export default nextConfig;
